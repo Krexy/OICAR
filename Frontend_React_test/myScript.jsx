21f4fb816d82a11c9e4e39cf542e2bf71d030ba4
Kr3xy <kresimirtajkun@gmail.com>
2020-03-15 18:09:12 +0100

    React instalation fail

---
+++ Frontend_React_test/myScript.jsx
@@ -0,0 +1,12 @@
+var MyComponent = React.createClass({
+  render :function(){
+    return(
+      <h2>Hello World!!</h2>
+    );
+  }
+});
+
+ReactDOM.render(
+    <MyComponent/>, document.getElementById('root')
+);
+  

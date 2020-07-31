0589d3d0f8aeb68187608f3ec661791e7504bf56
Kresica <n.kresica@gmail.com>
2020-07-31 17:44:47 +0200

    added web login/Logout tameplate

---
+++ WebApp/WebApp/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            //return View("Home/Index",model);
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpPost]
+        public ActionResult Index(LoginViewModel model, string returnUrl)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
